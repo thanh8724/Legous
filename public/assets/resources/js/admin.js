4dad89e9d45dddc012d032d927762ad93ceaba52
hoangthanhlagi <130956271+hoangthanhlagi@users.noreply.github.com>
2023-11-04 14:43:53 +0700

    update-admin
    update-admin

--- public/assets/resources/js/admin.js
+++ public/assets/resources/js/admin.js
@@ -1,4 +1,4 @@
-const body = document.querySelector("body"),
+const body = document.querySelector("body")
       modeToggle = body.querySelector(".mode-toggle");
       sidebar = body.querySelector("nav");
       sidebarToggle = body.querySelector(".sidebar-toggle");
@@ -12,6 +12,8 @@ let getStatus = localStorage.getItem("status");
 if(getStatus && getStatus ==="close"){
     sidebar.classList.toggle("close");
 }
+
+
 
 modeToggle.addEventListener("click", () =>{
     body.classList.toggle("dark");
@@ -30,3 +32,26 @@ sidebarToggle.addEventListener("click", () => {
         localStorage.setItem("status", "open");
     }
 })
+
+
+
+function openOptions (){
+   const  options = document.getElementById('dropdown-menu');
+   if (options.style.display === "none") {
+    options.style.display = "block";
+    options.style.opacity = '1';
+} else {
+    options.style.display = "none";
+    options.style.opacity = '0';
+  }
+
+}
+
+const options = document.querySelector('.dropdown-menu');
+const filterBtn = document.querySelector('#filter');
+console.log(options, filterBtn)
+if (options) {
+    filterBtn.onclick = () => {
+        options.classList.toggle('active');
+    }
+}
