889e02fe0563c00ce670a8b00e74ecf25cc99e38
ChuBe04 <118839446+HongSon04@users.noreply.github.com>
2023-11-25 14:51:46 +0700

    add comment

--- models/m_comment.php
+++ models/m_comment.php
@@ -4,5 +4,9 @@
         $sql = "SELECT * FROM comment WHERE id_product = $productId";
         return pdo_query($sql);
     }
+    function insertComment($id_user, $id_product, $username, $email, $content) {
+        // $sql = "INSERT INTO comment (id_user, id_product, username,
+        pdo_execute("INSERT INTO comment (`id_user`,`id_product`,`username`,`email`,`content`) VALUES(?,?,?,?,?)",$id_user, $id_product, $username, $email, $content);
 
+    }
 ?>
