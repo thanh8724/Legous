5f2f0c3743b7ddd8124fcce86b8e95253c0ec444
thanhnq <quocthanhn87@gmial.com>
2023-12-01 22:59:11 +0700

    fix delete account user

--- public/assets/resources/js/dark-light_mode.js
+++
@@ -1,5 +0,0 @@
-let circle_color = document.querySelector('#color__dark-light');
-function dark_lightMod() {
-    circle_color.style.transform = 'translateX(-10px)';
-    circle_color.style.backgroundColor = 'orange';
-}
